b7cb1600cee4e83642d4ee1dbb1f57fb47f1d15e
LucasBGarcia <barbozagarcia@yahoo.com.br>
2024-05-10 16:31:10 -0300

    troca rota api

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://bottrade-git-develop-lucasbgarcias-projects.vercel.app/",
 });
 
 export default api;
