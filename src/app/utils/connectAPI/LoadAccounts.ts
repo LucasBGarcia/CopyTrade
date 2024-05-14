6061b080ea9ef6082a01cc594816b11682ebbe14
LucasBGarcia <barbozagarcia@yahoo.com.br>
2024-05-14 13:40:39 -0300

    debug 7

--- src/app/utils/connectAPI/LoadAccounts.ts
+++ src/app/utils/connectAPI/LoadAccounts.ts
@@ -27,26 +27,26 @@ export async function LoadAccountsAPI(keysMaster: string, keysClientes: string) 
         console.log(splitMaster[0].trim())
         console.log(splitMaster[1].trim())
 
-        // if (splitMaster) {
-        //     console.log('valores iniciais antes')
-        //     try {
-        //         const valoresIniciais = await api.post('/get-account-balance-usdt', {
-        //             // apiKey: '4SYTGW0z943oiusiWL0NU89FWBkNAT9Fh7YaLSrhvmxeowQV8slnOVk5Ue5Qoxxo',
-        //             // apiSecret: 'bbFwPoARmSK6Pq3L23NRzAY6Fji9BkjjSAuIy82bl43BZ8vwM2UE5hJmncBvZBiP',
+        if (splitMaster) {
+            console.log('valores iniciais antes')
+            try {
+                const valoresIniciais = await api.post('/get-account-balance-usdt', {
+                    // apiKey: '4SYTGW0z943oiusiWL0NU89FWBkNAT9Fh7YaLSrhvmxeowQV8slnOVk5Ue5Qoxxo',
+                    // apiSecret: 'bbFwPoARmSK6Pq3L23NRzAY6Fji9BkjjSAuIy82bl43BZ8vwM2UE5hJmncBvZBiP',
 
-        //             apiKey: splitMaster[0].trim(),
-        //             apiSecret: splitMaster[1].trim(),
-        //         });
-        //         console.log('Valores iniciais:', valoresIniciais.data);
-        //         cookieStore.set({
-        //             name: "ValorInicialMaster",
-        //             value: JSON.stringify(valoresIniciais.data),
-        //             sameSite: 'strict'
-        //         })
-        //     } catch (err) {
-        //         console.log(err)
-        //     }
-        // }
+                    apiKey: splitMaster[0].trim(),
+                    apiSecret: splitMaster[1].trim(),
+                });
+                console.log('Valores iniciais:', valoresIniciais.data);
+                cookieStore.set({
+                    name: "ValorInicialMaster",
+                    value: JSON.stringify(valoresIniciais.data),
+                    sameSite: 'strict'
+                })
+            } catch (err) {
+                console.log(err)
+            }
+        }
 
         const splitClientes = keysClientes.split(',')
 
@@ -67,62 +67,11 @@ export async function LoadAccountsAPI(keysMaster: string, keysClientes: string) 
             value: JSON.stringify(traderMaster),
             sameSite: 'strict'
         })
-        // try {
-        //     console.log('listen key')
-
-        //     const listenKey = await api.post('/take-listen-key', {
-        //         apiKey: traderMaster.key
-        //     })
-        //     cookieStore.set({
-        //         name: "listen",
-        //         value: JSON.stringify(listenKey.data.listenKey),
-        //         sameSite: 'strict'
-        //     })
-        // } catch (err) {
-        //     console.log(err)
-        // }
-        // const listenKey = await TakeListenKey(traderMaster.key)
-
-        // await Promise.all(objeto.map(async (contas) => {
-        //     const AccountsBalance = await InfoAccountBalance()
-        //     console.log('accountBalance em LoadAccounts', AccountsBalance)
-        //     accountsBalance = AccountsBalance
-        // }))
-
-        // try {
-        //     console.log('accpimts ballance')
-        //     const AccountsBalance = await api.post('/get-All-account-balance-usdt', {
-        //         contasSTR: objeto
-        //     })
-        //     cookieStore.set({
-        //         name: "accountBalances",
-        //         value: JSON.stringify(AccountsBalance.data),
-        //         sameSite: 'strict'
-        //     })
-        //     return AccountsBalance.data
-        // } catch (err) {
-        //     console.log(err)
-        // }
-        const ret = await carrega_balances(true, objeto, splitMaster[0].trim(), splitMaster[1].trim(),)
-        console.log('keys recebidas em baixo', keysMaster, keysClientes)
-        return (ret)
-
-    } catch (e) {
-        return NextResponse.json({ e })
-    }
-}
-
-
-async function carrega_balances(ok: boolean, objeto: any, tradermasterKey: string, tradermasterSecret: string) {
-
-    const cookieStore = cookies()
-    if (ok) {
-
         try {
             console.log('listen key')
 
             const listenKey = await api.post('/take-listen-key', {
-                apiKey: tradermasterKey
+                apiKey: traderMaster.key
             })
             cookieStore.set({
                 name: "listen",
@@ -132,24 +81,13 @@ async function carrega_balances(ok: boolean, objeto: any, tradermasterKey: strin
         } catch (err) {
             console.log(err)
         }
+        // const listenKey = await TakeListenKey(traderMaster.key)
 
-        try {
-            const valoresIniciais = await api.post('/get-account-balance-usdt', {
-                // apiKey: '4SYTGW0z943oiusiWL0NU89FWBkNAT9Fh7YaLSrhvmxeowQV8slnOVk5Ue5Qoxxo',
-                // apiSecret: 'bbFwPoARmSK6Pq3L23NRzAY6Fji9BkjjSAuIy82bl43BZ8vwM2UE5hJmncBvZBiP',
-
-                apiKey: tradermasterKey,
-                apiSecret: tradermasterSecret,
-            });
-            console.log('Valores iniciais:', valoresIniciais.data);
-            cookieStore.set({
-                name: "ValorInicialMaster",
-                value: JSON.stringify(valoresIniciais.data),
-                sameSite: 'strict'
-            })
-        } catch (err) {
-            console.log(err)
-        }
+        // await Promise.all(objeto.map(async (contas) => {
+        //     const AccountsBalance = await InfoAccountBalance()
+        //     console.log('accountBalance em LoadAccounts', AccountsBalance)
+        //     accountsBalance = AccountsBalance
+        // }))
 
         try {
             console.log('accpimts ballance')
@@ -165,5 +103,67 @@ async function carrega_balances(ok: boolean, objeto: any, tradermasterKey: strin
         } catch (err) {
             console.log(err)
         }
+        // const ret = await carrega_balances(true, objeto, splitMaster[0].trim(), splitMaster[1].trim(),)
+        // console.log('keys recebidas em baixo', keysMaster, keysClientes)
+        // return (ret)
+
+    } catch (e) {
+        return NextResponse.json({ e })
     }
 }
+
+
+// async function carrega_balances(ok: boolean, objeto: any, tradermasterKey: string, tradermasterSecret: string) {
+
+//     const cookieStore = cookies()
+//     if (ok) {
+
+//         try {
+//             console.log('listen key')
+
+//             const listenKey = await api.post('/take-listen-key', {
+//                 apiKey: tradermasterKey
+//             })
+//             cookieStore.set({
+//                 name: "listen",
+//                 value: JSON.stringify(listenKey.data.listenKey),
+//                 sameSite: 'strict'
+//             })
+//         } catch (err) {
+//             console.log(err)
+//         }
+
+//         try {
+//             const valoresIniciais = await api.post('/get-account-balance-usdt', {
+//                 // apiKey: '4SYTGW0z943oiusiWL0NU89FWBkNAT9Fh7YaLSrhvmxeowQV8slnOVk5Ue5Qoxxo',
+//                 // apiSecret: 'bbFwPoARmSK6Pq3L23NRzAY6Fji9BkjjSAuIy82bl43BZ8vwM2UE5hJmncBvZBiP',
+
+//                 apiKey: tradermasterKey,
+//                 apiSecret: tradermasterSecret,
+//             });
+//             console.log('Valores iniciais:', valoresIniciais.data);
+//             cookieStore.set({
+//                 name: "ValorInicialMaster",
+//                 value: JSON.stringify(valoresIniciais.data),
+//                 sameSite: 'strict'
+//             })
+//         } catch (err) {
+//             console.log(err)
+//         }
+
+//         try {
+//             console.log('accpimts ballance')
+//             const AccountsBalance = await api.post('/get-All-account-balance-usdt', {
+//                 contasSTR: objeto
+//             })
+//             cookieStore.set({
+//                 name: "accountBalances",
+//                 value: JSON.stringify(AccountsBalance.data),
+//                 sameSite: 'strict'
+//             })
+//             return AccountsBalance.data
+//         } catch (err) {
+//             console.log(err)
+//         }
+//     }
+// }
